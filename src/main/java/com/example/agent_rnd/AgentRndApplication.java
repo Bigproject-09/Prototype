dce310d6c511d7bba3822a4251b683d58c4ea8b8
gonago777 <hjjh5019@naver.com>
2026-01-21 17:24:22 +0900

    오류수정

--- src/main/java/com/example/agent_rnd/AgentRndApplication.java
+++ src/main/java/com/example/agent_rnd/AgentRndApplication.java
@@ -1,3 +1,25 @@
+//package com.example.agent_rnd;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+//import com.example.agent_rnd.config.ExternalDataGoProperties;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//
+//@EnableConfigurationProperties(ExternalDataGoProperties.class)
+//@ConfigurationPropertiesScan
+//@EnableJpaAuditing
+//@SpringBootApplication
+//public class AgentRndApplication {
+//
+//    public static void main(String[] args) {
+//
+//        SpringApplication.run(AgentRndApplication.class, args);
+//    }
+//
+//}
+
 package com.example.agent_rnd;
 
 import org.springframework.boot.SpringApplication;
@@ -17,5 +39,5 @@ public class AgentRndApplication {
 
         SpringApplication.run(AgentRndApplication.class, args);
     }
-
 }
+
