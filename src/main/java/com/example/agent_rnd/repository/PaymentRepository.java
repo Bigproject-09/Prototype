14558a56dc1502decace6592aca4ca8175436d17
geunwoo <garroid@nver.com>
2026-01-27 13:53:39 +0900

    User 수정, payments 구현 - API 키는 차후 추가 필요

---
+++ src/main/java/com/example/agent_rnd/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.example.agent_rnd.repository;
+
+import com.example.agent_rnd.domain.payment.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    // 주문번호로 찾기
+    Optional<Payment> findByMerchantUid(String merchantUid);
+
+    // 포트원 번호로 찾기
+    Optional<Payment> findByImpUid(String impUid);
+}
